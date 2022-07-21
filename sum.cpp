bfbd84a3978c3e629c5bf5911c28c0374763ad1c
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-07-21 08:48:02 +0200

    another exercise + edit welcome spelling

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* swe110,sum.cpp
+this file is to calculate the sum of 29 + 30
+shehab aboshady 2021/06754
+version 1.0 21/7/2022  */
+int main() {
+cout<<"the sum of 29 + 30 =\n" << 29 + 30 << "\n";
+
+return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-/* swe110,welome.cpp
+/* swe110,welcome.cpp
 this file is to welcoming the user
 shehab aboshady 2021/06754
 version 1.0 21/7/2022  */
