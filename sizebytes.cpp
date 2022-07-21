e952568dda5a27ebea3d0399fc29e25f3368f175
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-07-21 09:11:36 +0200

    3rd exercise

---
+++ sizebytes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+/* swe110,sizebytes.cpp
+this file is to calculate the bytes on the data types
+shehab aboshady 2021/06754
+version 1.0 21/7/2022  */
+int main() {
+cout<<"the bytes on every data type :\n";
+cout<<"the size of int =\n";
+ cout<< sizeof(int) << " bytes\n";
+ //every data type have the number of bytes under it
+ cout<<"the size of double =\n";
+ cout<< sizeof(double) << " bytes\n";
+ cout<<"the size of char =\n";
+ cout<< sizeof(char) << " bytes\n";
+ cout<<"the size of float =\n";
+ cout<< sizeof(float) << " bytes\n";
+ cout<<"the size of bool =\n";
+ cout<< sizeof(bool) << " bytes\n";
+ cout<<"the size of short =\n";
+ cout<< sizeof(short) << " bytes\n";
+ cout<<"the size of long =\n";
+ cout<< sizeof(long) << " bytes\n";
+ cout<<"the size of long long =\n";
+ cout<< sizeof(long long) << " bytes\n";
+return 0;
+}
