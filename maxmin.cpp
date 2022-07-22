aaa847986d073b3f2800e21a060a15980751318c
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-07-22 07:17:39 +0200

    exercise 5

---
+++ maxmin.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+/*swe110,maxmin.cpp
+this file to see the max and min of data types
+shehab aboshady 2021/06754
+version 1.0, 22/7/2022 */
+int main(){
+cout<<"the maximum limit of int data type =\t" << INT_MAX << "\n";
+cout<<"the minimum limit of int data type =\t" << INT_MIN << "\n";
+cout<<"the maximum limit of unsigned int data type =\t" << UINT_MAX << "\n";
+cout<<"the maximum limit of long long  data type =\t" << LLONG_MAX << "\n";
+cout<<"the minimum limit of long long data type =\t" << LLONG_MIN << "\n";
+cout<<"the maximum limit of unsigned long long  data type =\t" << ULLONG_MAX << "\n";
+cout<<"the bits contain in char  data type =\t" << CHAR_BIT << "\n";
+cout<<"the maximum limit of char  data type =\t" << CHAR_MAX << "\n";
+cout<<"the minimum limit of char  data type =\t" << CHAR_MIN << "\n";
+cout<<"the maximum limit of signed char data type =\t" << SCHAR_MAX << "\n";
+cout<<"the minimum limit of signed char  data type =\t" << SCHAR_MIN << "\n";
+cout<<"the maximum limit of unsigned char data type =\t" << UCHAR_MAX << "\n";
+cout<<"the maximum limit of short data type =\t" << SHRT_MAX << "\n";
+cout<<"the minimum limit of short data type =\t" << SHRT_MIN << "\n";
+cout<<"the maximum limit of unsigned short data type =\t" << USHRT_MAX << "\n";
+
+
+
+
+
+
+
+}
