8662ac4a73da9417074f3c1a3c95a5b80cf48a61
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-07-22 06:48:22 +0200

    exercise 4

---
+++ var_sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+/*swe110,var_sum.cpp
+this file for sum 2 number as variables
+shehab aboshady 2021/06754
+version 1.0, 22/7/2022 */
+int main(){
+int num1=29;
+int num2=30;
+cout<<"the sum of num1 and num2 =\t"<< num1+num2<<"\n";
+    return 0;
+}
