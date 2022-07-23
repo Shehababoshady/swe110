eee8076505302d728968680412057f14753c3584
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-07-23 07:52:49 +0200

    exercise 6

---
+++ limit.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+/* swe 110, limit.cpp
+this file to check whether the primitive values crossing the limits or not
+shehab aboshady 2021/06754
+version 1.0 , 23/7/2022 */
+int main() {
+char gender = 'F';
+bool married = true;
+short sons = 2;
+short year = 2009;
+int salary =1500000;
+double height = 79.48;
+float gpa = 4.69;
+long saldrawn=12047235;     //salary drawn up to
+long long balance = 995324987;
+cout<<"the gender is :\t"<< gender << "\n";
+cout<<"is she married? :\t"<< married << "\n";
+cout<<"number of sons she has :\t"<< sons << "\n";
+cout<<"year of the appointment :\t"<< year << "\n";
+cout<<"salary for the year :\t"<< salary << "\n";
+cout<<"height is :\t"<< height << "\n";
+cout<<"GPA is :\t"<< gpa << "\n";
+cout<<"salary draen up to :\t"<< saldrawn << "\n";
+cout<<" blance till :\t"<< balance << "\n";
+
+
+
+return 0;
+
+}
