a143ba04a0f9c95e819dff6877f39a770e3a7208
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-08-03 17:40:54 +0200

    exersice

---
+++ fnatural.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/*
+swe110,fnatural.cpp
+a program to find the first 10 natural numbers
+shehab aboshady 2021/06754
+version 1.0 , 3/8/2022 */
+int main()
+{
+
+    cout << "the natural 10 number is :\n";
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
