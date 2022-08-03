833cd1f2574c6e71dd75c9b9e90403153d65ace5
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-08-03 20:35:52 +0200

    lap 4 exersice 4

---
+++ lab4e4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+/*
+swe110,lab4e4.cpp
+this file for lab 4 exersice 4
+shehab aboshady 2021/06754
+version 1.0 , 3/8/2022 */
+int main()
+{
+    int num;
+    cout << "enter a number\n";
+    cin >> num;
+    num *= -1;
+    cout << num << "x" << 2 << "=" << num * 2 << "\n";
+}
