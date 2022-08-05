655f315822e1fd6d528772368a739299c69da3c3
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-08-05 14:12:04 +0200

    date

--- lab5e4.cpp
+++ lab5e4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 swe110,lab5e4.cpp
 this file for lab 5 exersice 4
 shehab aboshady 2021/06754
-version 1.0 , 3/8/2022 */
+version 1.0 , 5/8/2022 */
 int main()
 {
     int num, sum = 0, result = 0;
