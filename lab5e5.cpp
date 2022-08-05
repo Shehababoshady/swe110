f03830a7ae3965637137a55363796c09fd7d20d7
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-08-05 13:55:38 +0200

    lab 5 exersice 5

---
+++ lab5e5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+swe110,lab5e5.cpp
+this file for lab 5 exersice 5
+shehab aboshady 2021/06754
+version 1.0 , 3/8/2022 */
+int main()
+{
+    int num, sum = 0;
+    do
+    {
+        cout << "enter a number\n";
+        cin >> num;
+
+        if (num <= 15)
+        {
+            sum += num;
+        }
+    } while (num <= 15);
+    cout << "the sum of the numbers is: " << sum << "\n";
+}
