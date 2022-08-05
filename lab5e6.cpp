8c9bc7cfc4f48e9a44e8fc500cbef08648d06f49
Shehababoshady <shehab2106754@miuegypt.edu.eg>
2022-08-05 14:10:28 +0200

    lab 5 exercise 6

---
+++ lab5e6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+/*
+swe110,lab5e6.cpp
+this file for lab 5 exersice 6
+shehab aboshady 2021/06754
+version 1.0 , 3/8/2022 */
+int main()
+{
+    int table;
+    cout << "enter a number: ";
+    cin >> table;
+    for (int i = 1; i <= table; i++)
+    {
+        for (int x = 1; x <= table; x++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
